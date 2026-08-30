1f873527bed19f6d7f37e5babaf5f86e237560e4
ksp-KWON <kspcl@naver.com>
2026-08-30 18:22:18 +0900

    fix: restore full-height aside track to enable true CSS position sticky scrolling

--- src/components/SmartStickyLayout.tsx
+++ src/components/SmartStickyLayout.tsx
@@ -9,16 +9,16 @@ interface Props {
 
 export default function SmartStickyLayout({ mainContent, sidebarContent }: Props) {
   return (
-    <div className="mx-auto w-full sm:w-[92vw] xl:w-[85vw] max-w-7xl px-2 sm:px-5 py-6 sm:py-8 flex flex-col lg:flex-row gap-6 lg:gap-8 items-start">
+    <div className="mx-auto w-full sm:w-[92vw] xl:w-[85vw] max-w-7xl px-2 sm:px-5 py-6 sm:py-8 flex flex-col lg:flex-row gap-6 lg:gap-8 items-stretch">
       {/* 본문 영역 (72% 비율) */}
-      <main className="w-full lg:w-[72%] flex-1 min-w-0 transition-all duration-300">
+      <main className="w-full lg:w-[72%] flex-1 min-w-0">
         {mainContent}
       </main>
 
-      {/* 사이드바 영역 (28% 비율, self-start 기반 정밀 스마트 스티키) */}
+      {/* 사이드바 영역 (28% 비율, 부모 높이 전폭 스트레치 + 내부 100% sticky 고정) */}
       {sidebarContent && (
-        <aside className="hidden lg:block w-full lg:w-[28%] sticky top-[80px] self-start transition-all duration-300 lg:px-0">
-          <div className="w-full">
+        <aside className="hidden lg:block w-full lg:w-[28%] relative">
+          <div className="sticky top-[80px] w-full">
             {sidebarContent}
           </div>
         </aside>
